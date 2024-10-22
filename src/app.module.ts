cbe6cbdaffc541eb22d1ba57ef2a133ec1c7cbba
velyn666 <ni1722650@gmail.com>
2024-10-22 09:16:51 +0900

    우

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { MysqlSequelizeModule } from './mysql_sequelize/mysql_sequelize.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, UserModule, PostModule, MysqlSequelizeModule],
   controllers: [AppController],
   providers: [AppService],
 })

c192f137ee23e19994266fba5527611be2bc6680
velyn666 <ni1722650@gmail.com>
2024-10-22 09:11:10 +0900

    유유유유유유 슈퍼 유효성

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
+
+  //서버에 Request가 들어올 때 유효성 검사를 해줌
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }

3486c64afcfa7ce413c336d007b3c04d714ca32f
velyn666 <ni1722650@gmail.com>
2024-10-22 09:02:48 +0900

    cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();

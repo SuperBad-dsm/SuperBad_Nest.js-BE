8fa1c8f54f0bf1d2c69a9cb300fd919d0ff1c505
velyn666 <ni1722650@gmail.com>
2024-10-23 20:26:48 +0900

    파일 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
-import { UserModule } from './user/user.module';
-import { PostModule } from './post/post.module';
-import { MysqlSequelizeModule } from './mysql_sequelize/mysql_sequelize.module';
+import { ProductsModel } from './domain/entities/products.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './product/products.module';
+import { UserModule } from './domain/user/users.module';
 
 @Module({
-  imports: [PostsModule, UserModule, PostModule, MysqlSequelizeModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProductsModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      //db type
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [ProductsModel],
+      //nest js에서 작성하는 type orm 코드와 데이터베이스 자동으로 싱크를 맞출것인가.
+      //개발 환경에서는 되어 있는 게 편하지만, 프로덕션 환경에서는 db 구조가 마음대로 바뀔 환경이 있어 false 해야함.
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}

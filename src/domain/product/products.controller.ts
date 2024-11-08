1949f5179eefe633ed11ffa19cc108fbc2feb491
velyn666 <ni1722650@gmail.com>
2024-11-08 10:50:36 +0900

    add auth guard

--- src/domain/product/products.controller.ts
+++ src/domain/product/products.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('products')
 export class ProductsController {
@@ -7,12 +8,14 @@ export class ProductsController {
 
   //모든 상품 가져오기
   @Get()
+  @UseGuards(JwtAuthGuard)
   getProducts() {
     return this.productsService.getAllProducts();
   }
 
   //특정 상품 정보 가져오기
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   getProduct(@Param('id') id: string) {
     return this.productsService.getProductById(+id);
   }
